c6317ad0df09e38d754179dccac6040753b301cc
AndriusBendoraitis <andrius.bendoraitis1985@gmail.com>
2019-12-07 11:19:47 +0200

    added type

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,11 @@
 public class Car {
 
     private String name;
+    private String type;
+
+    public String getType() {
+        return type;
+    }
 
     public Car(String name) {
         this.name = name;
